2118e8d2e56a015b0e2471852760e512ed2772e1
Romain Arnaud <rarnaud@redhat.com>
2025-07-03 16:23:22 -0400

    baby steps

--- ingestion-pipeline/src/ingestion_pipeline/pipelines/pipelines.py
+++ ingestion-pipeline/src/ingestion_pipeline/pipelines/pipelines.py
@@ -19,19 +19,27 @@ def s3_pipeline(pipeline_name: str, llamastack_base_url: str):
                 'REGION': 'REGION'
         }
 
+        pipeline_tasks=[]
+
         fetch_task = tasks.fetch_from_s3()
         fetch_task.set_caching_options(False)
+        pipeline_tasks.append(fetch_task)
 
         store_task = tasks.store_documents(
             llamastack_base_url=llamastack_base_url,
             input_dir=fetch_task.outputs["output_dir"]
         )
         store_task.set_caching_options(False)
+        pipeline_tasks.append(store_task)
 
-        provenance_task = tasks.generate_provenance(pipeline_name)
-        provenance_task.after(store_task)
+        # echo_task = tasks.echo()
+        # echo_task.after(store_task)
+        # pipeline_tasks.append(echo_task)
 
-        for task in (fetch_task, store_task, provenance_task):
+        # provenance_task = tasks.generate_provenance(pipeline_name)
+        # provenance_task.after(store_task)
+
+        for task in pipeline_tasks:
             kubernetes.use_secret_as_env(
                 task=task,
                 secret_name=pipeline_name,
--- ingestion-pipeline/src/ingestion_pipeline/pipelines/tasks.py
+++ ingestion-pipeline/src/ingestion_pipeline/pipelines/tasks.py
@@ -219,42 +219,48 @@ def store_documents(llamastack_base_url: str, input_dir: dsl.InputPath()):
         print("Embedding insert failed:", e)
         raise Exception(f"Failed to insert documents into vector DB: {e}")
 
+# @dsl.component(
+#     base_image=BASE_IMAGE,
+#     )
+# def echo():
+#     print()
+#     print("Hello World!")
+#     print()
 
+# @dsl.component(
+#     base_image=BASE_IMAGE,
+#     packages_to_install=[
+#         "json",
+#     ])
+# def generate_provenance(pipeline_name: str):
+#     import json
+#     provenance = {
+#         # Standard attestation fields:
+#         "_type": "https://in-toto.io/Statement/v1",
+#         "subject": pipeline_name,
 
-@dsl.component(
-    base_image=BASE_IMAGE,
-    packages_to_install=[
-        "json",
-    ])
-def generate_provenance(pipeline_name: str):
-    import json
-    provenance = {
-        # Standard attestation fields:
-        "_type": "https://in-toto.io/Statement/v1",
-        "subject": pipeline_name,
-
-        # Predicate:
-        "predicateType": "https://slsa.dev/provenance/v1",
-        "predicate": {
-            "buildDefinition": {
-                "buildType": "",
-                "externalParameters": {},
-                "internalParameters": {},
-                "resolvedDependencies": [],
-            },
-            "runDetails": {
-                "builder": {
-                    "id": "",
-                    "builderDependencies": [],
-                    "version": {},
-                },
-                "metadata": {
-                    "invocationId": "",
-                    "startedOn": "",
-                    "finishedOn": "",
-                },
-                "byproducts": [],
-            }
-        }
-    }
-    print(json.dumps(provenance, indent=2))
+#         # Predicate:
+#         "predicateType": "https://slsa.dev/provenance/v1",
+#         "predicate": {
+#             "buildDefinition": {
+#                 "buildType": "",
+#                 "externalParameters": {},
+#                 "internalParameters": {},
+#                 "resolvedDependencies": [],
+#             },
+#             "runDetails": {
+#                 "builder": {
+#                     "id": "",
+#                     "builderDependencies": [],
+#                     "version": {},
+#                 },
+#                 "metadata": {
+#                     "invocationId": "",
+#                     "startedOn": "",
+#                     "finishedOn": "",
+#                 },
+#                 "byproducts": [],
+#             }
+#         }
+#     }
+#     print(json.dumps(provenance, indent=2))
